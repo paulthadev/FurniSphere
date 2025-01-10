fed024416cec0493b0bd2640c1593b368131d779
fadayopaul <paulfadayo@yahoo.com>
2025-01-10 13:24:27 +0100

    feat: add NavLinks component for improved navigation in the Navbar

---
+++ src/components/navbar/NavLinks.tsx
@@ -0,0 +1,22 @@
+import { links } from "@/utils";
+import { NavLink } from "react-router-dom";
+
+function NavLinks(): JSX.Element {
+  return (
+    <div className="hidden items-center justify-center gap-x-4 lg:flex">
+      {links.map((link) => (
+        <NavLink
+          to={link.href}
+          key={link.label}
+          className={({ isActive }) =>
+            `border- rounded-lg border px-2 py-1 capitalize tracking-wide ${isActive ? "text-primary" : "font-light"}`
+          }
+        >
+          {link.label}
+        </NavLink>
+      ))}
+    </div>
+  );
+}
+
+export default NavLinks;
