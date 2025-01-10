a5a905fc2d0415363d5d904b2af5814f2604bbfe
fadayopaul <paulfadayo@yahoo.com>
2025-01-10 22:52:49 +0100

    feat: update CartButton to use ShoppingCartIcon and display item count

--- src/components/navbar/CartButton.tsx
+++ src/components/navbar/CartButton.tsx
@@ -1,11 +1,26 @@
-import { CarTaxiFrontIcon } from "lucide-react";
+import { ShoppingCartIcon } from "lucide-react";
 import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
 
 function CartButton() {
+  const numItemsInCart = 5;
   return (
     <div>
-      <Button size="icon">
-        <CarTaxiFrontIcon />
+      <Button
+        size="icon"
+        variant="outline"
+        asChild
+        className="relative flex items-center justify-center"
+      >
+        <Link to="/cart">
+          <ShoppingCartIcon className="text-primary" />
+
+          <span
+            className={`absolute -right-3 -top-3 flex h-6 w-6 items-center justify-center rounded-full bg-primary text-xs text-white dark:bg-primary-foreground dark:text-primary`}
+          >
+            {numItemsInCart}
+          </span>
+        </Link>
       </Button>
     </div>
   );
