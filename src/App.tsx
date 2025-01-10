c1723a8e6571855e932044453fe99ac2db0447a3
fadayopaul <paulfadayo@yahoo.com>
2025-01-10 08:56:38 +0100

    feat: enhance App component to display user name and include Cart component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { useAppSelector } from "./hooks";
+import {
+  About,
+  Cart,
+  Checkout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Orders,
+  Products,
+  Register,
+  SingleProduct,
+} from "./pages";
 
 function App() {
+  const { name } = useAppSelector((state) => state.userState);
   return (
     <div className=" font-bold grid place-content-center h-screen">
-      Welcome to FurniSphere
+      Welcome to FurniSphere, {name}
       <Button
         variant="default"
         size="sm"
@@ -11,6 +26,7 @@ function App() {
       >
         Click Here
       </Button>
+      <Cart />
     </div>
   );
 }
