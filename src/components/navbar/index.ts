4d5f1b39dba7c8161c1b691d95277a567ca21d36
fadayopaul <paulfadayo@yahoo.com>
2025-01-10 12:37:27 +0100

    feat: export additional components from Navbar index for improved accessibility

---
+++ src/components/navbar/index.ts
@@ -0,0 +1,5 @@
+export { default as CartButton } from "./CartButton";
+export { default as LinksDropdown } from "./LinksDropdown";
+export { default as Logo } from "./Logo";
+export { default as ModeToggle } from "./ModeToggle";
+export { default as NavLinks } from "./NavLinks";
