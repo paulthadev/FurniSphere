843ff861b67c6dd58ca254bc8c47c37227f95611
fadayopaul <paulfadayo@yahoo.com>
2025-01-10 23:00:52 +0100

    fix: update CartButton icon color for dark mode compatibility

--- src/components/navbar/CartButton.tsx
+++ src/components/navbar/CartButton.tsx
@@ -13,7 +13,7 @@ function CartButton() {
         className="relative flex items-center justify-center"
       >
         <Link to="/cart">
-          <ShoppingCartIcon className="text-primary" />
+          <ShoppingCartIcon className="text-primary dark:text-white" />
 
           <span
             className={`absolute -right-3 -top-3 flex h-6 w-6 items-center justify-center rounded-full bg-primary text-xs text-white dark:bg-primary-foreground dark:text-primary`}
