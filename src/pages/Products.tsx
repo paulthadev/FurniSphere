bce773ed774bc90753827c98f626c52a53ec6fb0
fadayopaul <paulfadayo@yahoo.com>
2025-01-11 21:46:51 +0100

    feat: implement product loading and display components in Products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,5 +1,25 @@
+/* eslint-disable react-refresh/only-export-components */
+import { authApi } from "@/api/axios";
+import {
+  Filters,
+  PaginationContainer,
+  ProductsContainer,
+} from "@/components/product";
+import { type ProductsResponse } from "@/types";
+
+export const loader = async (): Promise<ProductsResponse> => {
+  const response = await authApi.get("/products");
+  return { ...response.data };
+};
+
 function Products() {
-  return <div>Products</div>;
+  return (
+    <>
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
+    </>
+  );
 }
 
 export default Products;
