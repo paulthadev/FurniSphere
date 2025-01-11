d61a5f43da60149ccdb2db40e32bcdae5bd89e7b
fadayopaul <paulfadayo@yahoo.com>
2025-01-11 04:46:10 +0100

    feat: enhance About component with new layout and content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,24 @@
 function About() {
-  return <div className="">About</div>;
+  return (
+    <section>
+      <h1 className="flex flex-wrap items-center justify-center gap-2 text-4xl font-bold leading-none tracking-wide sm:gap-x-6 sm:text-6xl">
+        We love
+        <span className="rounded-lg bg-primary px-4 py-2 text-white">
+          FurniSphere
+        </span>
+      </h1>
+
+      <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 tracking-wide">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+        quisquam voluptas ex non odio perferendis modi debitis qui,
+        reprehenderit ad, inventore facere consequatur quasi obcaecati nostrum
+        quas voluptatem aut deserunt. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Est rerum incidunt quas excepturi eveniet iure
+        laudantium commodi vel quod exercitationem labore modi officiis ipsam
+        iusto sint, nesciunt ea. Labore, et.
+      </p>
+    </section>
+  );
 }
 
 export default About;
