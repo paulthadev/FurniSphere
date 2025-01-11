17dc836898149a3b31f049fda95bf9e9a208976b
fadayopaul <paulfadayo@yahoo.com>
2025-01-11 17:42:07 +0100

    feat: implement ProductsGrid component to display a grid of products with links and pricing

--- src/components/landing/ProductsGrid.tsx
+++ src/components/landing/ProductsGrid.tsx
@@ -1,5 +1,39 @@
+import { Product } from "@/types";
+import { formatAsDollars } from "@/utils";
+import { Link, useLoaderData } from "react-router-dom";
+import { Card, CardContent } from "../ui/card";
+
 function ProductsGrid() {
-  return <div>ProductsGrid</div>;
+  const { data: products } = useLoaderData();
+
+  return (
+    <div className="grid gap-4 pt-12 sm:grid-cols-2 md:grid-cols-3">
+      {products.map((product: Product) => {
+        const { id } = product;
+        const { title, image, price } = product.attributes;
+        const dollarAmount = formatAsDollars(price);
+
+        return (
+          <Link to={`/products/${id}`}>
+            <Card>
+              <CardContent>
+                <img
+                  src={image}
+                  alt={title}
+                  className="h-64 w-full rounded-md object-cover md:h-48"
+                />
+
+                <div className="mt-4 text-center">
+                  <h2 className="text-xl font-semibold capitalize">{title}</h2>
+                  <p className="mt-2 font-light text-primary">{dollarAmount}</p>
+                </div>
+              </CardContent>
+            </Card>
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ProductsGrid;
