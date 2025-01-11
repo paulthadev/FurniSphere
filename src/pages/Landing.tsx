4b3f78bcf2de8774424e04311c22d1cc861c61f8
fadayopaul <paulfadayo@yahoo.com>
2025-01-11 16:11:33 +0100

    feat: implement loader function to fetch featured products for Landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,14 @@
+/* eslint-disable react-refresh/only-export-components */
+import { type LoaderFunction } from "react-router-dom";
+import { authApi } from "@/api/axios";
 import { Hero, FeaturedProduct } from "@/components/landing";
+import { ProductsResponse } from "@/types";
+
+export const loader: LoaderFunction = async (): Promise<ProductsResponse> => {
+  const featuredProductUrl = "/products?featured=true";
+  const response = await authApi.get<ProductsResponse>(featuredProductUrl);
+  return { ...response.data };
+};
 
 function Landing() {
   return (
