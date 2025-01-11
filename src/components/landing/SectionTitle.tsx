aebc78ea438fa6ce959343c83d0067866435e3b4
fadayopaul <paulfadayo@yahoo.com>
2025-01-11 16:48:14 +0100

    feat: update SectionTitle component to accept text prop and include separator

--- src/components/landing/SectionTitle.tsx
+++ src/components/landing/SectionTitle.tsx
@@ -1,5 +1,14 @@
-function SectionTitle() {
-  return <div>SectionTitle</div>;
+import { Separator } from "../ui/separator";
+
+function SectionTitle({ text }: { text: string }) {
+  return (
+    <div>
+      <h2 className="mb-8 text-3xl font-medium capitalize tracking-wider">
+        {text}
+      </h2>
+      <Separator />
+    </div>
+  );
 }
 
 export default SectionTitle;
