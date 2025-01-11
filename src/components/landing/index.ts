57ac313d38ca8a70b2a267ddce2ac1e6da2d7823
fadayopaul <paulfadayo@yahoo.com>
2025-01-11 16:48:09 +0100

    fix: update export for SectionTitle component to reference the correct file

--- src/components/landing/index.ts
+++ src/components/landing/index.ts
@@ -2,4 +2,4 @@ export { default as Hero } from "./Hero";
 export { default as HeroCarousel } from "./HeroCarousel";
 export { default as FeaturedProduct } from "./FeaturedProduct";
 export { default as ProductsGrid } from "./ProductsGrid";
-export { default as SectionTitle } from "./Hero";
+export { default as SectionTitle } from "./SectionTitle";
