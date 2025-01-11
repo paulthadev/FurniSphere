efe1339a04173ae213e332f8ef67c77388ce6031
fadayopaul <paulfadayo@yahoo.com>
2025-01-11 18:33:59 +0100

    feat: implement HeroCarousel component with image carousel functionality

--- src/components/landing/HeroCarousel.tsx
+++ src/components/landing/HeroCarousel.tsx
@@ -1,5 +1,38 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import { HeroImg1, HeroImg2, HeroImg3, HeroImg4 } from "@/assets";
+
+const carouselImages = [HeroImg1, HeroImg2, HeroImg3, HeroImg4];
+
 function HeroCarousel() {
-  return <div>HeroCarousel</div>;
+  return (
+    <Carousel className="hidden lg:block">
+      <CarouselContent>
+        {carouselImages.map((image, index) => (
+          <CarouselItem key={index}>
+            <Card>
+              <CardContent className="p-2">
+                <img
+                  src={image}
+                  alt={`hero-image-${index + 1}`}
+                  className="h-96 w-full rounded-md object-cover"
+                />
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
 }
 
 export default HeroCarousel;
