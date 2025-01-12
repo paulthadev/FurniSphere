350084c826c53f9004bc79faad8a0488eb838525
fadayopaul <paulfadayo@yahoo.com>
2025-01-12 21:36:26 +0100

    feat: use fallback for label in FormInput component for better usability

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -11,7 +11,7 @@ type FormInputProps = {
 function FormInput({ name, defaultValue, type, label }: FormInputProps) {
   return (
     <div className="mb-2">
-      <Label htmlFor={name}>{label}</Label>
+      <Label htmlFor={name}>{label || name}</Label>
       <Input type={type} name={name} id={name} defaultValue={defaultValue} />
     </div>
   );
