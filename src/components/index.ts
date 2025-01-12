b76c8deccdf095dcfa99859250e041c746b1e5c5
fadayopaul <paulfadayo@yahoo.com>
2025-01-12 23:20:58 +0100

    refactor: remove FormInput and FormSelect exports from index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,5 +3,3 @@ export { default as Footer } from "./Footer";
 export { default as Navbar } from "./Navbar";
 export { default as ErrorElement } from "./ErrorElement";
 export { default as Loading } from "./Loading";
-export { default as FormInput } from "./FormInput";
-export { default as FormSelect } from "./FormSelect";
