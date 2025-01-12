652a7376892720527595adf4ee867cdb1399e03c
fadayopaul <paulfadayo@yahoo.com>
2025-01-12 21:04:04 +0100

    feat: integrate search parameter into Filters component for improved product search functionality

--- src/components/product/Filters.tsx
+++ src/components/product/Filters.tsx
@@ -1,14 +1,16 @@
-import { Form, Link } from "react-router-dom";
+import { Form, Link, useLoaderData } from "react-router-dom";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 
 function Filters() {
+  const { params } = useLoaderData();
+  const { search } = params;
   return (
     <Form className="grid items-center gap-4 rounded-md border px-8 py-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       <div className="mb-2">
         <Label htmlFor="search">Search Product</Label>
-        <Input type="text" name="search" id="search" defaultValue="" />
+        <Input type="text" name="search" id="search" defaultValue={search} />
       </div>
 
       <Button type="submit" className="mb-2 self-end" size="sm">
