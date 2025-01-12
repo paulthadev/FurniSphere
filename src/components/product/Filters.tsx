7e96ac7dc749260ed1199ef578ba2d7cb5423f8c
fadayopaul <paulfadayo@yahoo.com>
2025-01-12 23:21:51 +0100

    feat: enhance Filters component with category, company, and order selection

--- src/components/product/Filters.tsx
+++ src/components/product/Filters.tsx
@@ -1,10 +1,12 @@
 import { Form, Link, useLoaderData } from "react-router-dom";
 import { Button } from "../ui/button";
-import FormInput from "../FormInput";
+import { FormInput, FormRange, FormSelect } from "@/components/form";
 
 function Filters() {
-  const { params } = useLoaderData();
-  const { search } = params;
+  const { meta, params } = useLoaderData();
+  const { search, category, company, order, price } = params;
+  const { categories, companies } = meta;
+
   return (
     <Form className="grid items-center gap-4 rounded-md border px-8 py-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       <FormInput
@@ -13,10 +15,40 @@ function Filters() {
         type="text"
         defaultValue={search}
       />
+
+      {/* CATEGORIES */}
+      <FormSelect
+        label="select category"
+        name="category"
+        options={categories}
+        defaultValue={category}
+      />
+
+      {/* COMPANY */}
+      <FormSelect
+        label="select company"
+        name="company"
+        options={companies}
+        defaultValue={company}
+      />
+
+      {/* ORDER */}
+      <FormSelect
+        label="order by"
+        name="order"
+        options={["a-z", "z-a", "high", "low"]}
+        defaultValue={order}
+      />
+
+      {/* RANGE */}
+      <FormRange label="price" name="price" defaultValue={price} />
+
+      {/* SEARCH BUTTON */}
       <Button type="submit" className="mb-2 self-end" size="sm">
         Search
       </Button>
 
+      {/* RESET BUTTON */}
       <Button
         type="button"
         asChild
