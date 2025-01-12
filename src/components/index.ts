613c303941f9592f6b586ce290624b237f9378a9
fadayopaul <paulfadayo@yahoo.com>
2025-01-12 22:04:50 +0100

    feat: export FormSelect component from index file for improved accessibility

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Navbar } from "./Navbar";
 export { default as ErrorElement } from "./ErrorElement";
 export { default as Loading } from "./Loading";
 export { default as FormInput } from "./FormInput";
+export { default as FormSelect } from "./FormSelect";
