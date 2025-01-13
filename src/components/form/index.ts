9504ff8d2f6343697095deb87cfc38a3f75753f7
fadayopaul <paulfadayo@yahoo.com>
2025-01-13 01:09:36 +0100

    feat: export FormCheckbox component from form index

--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -1,3 +1,4 @@
 export { default as FormInput } from "./FormInput";
 export { default as FormSelect } from "./FormSelect";
 export { default as FormRange } from "./FormRange";
+export { default as FormCheckbox } from "./FormCheckbox";
